458cd0b08e22e60588b95d1f78615d4b0691366f
coscoaj1 <coscoanthony1@gmail.com>
2021-12-11 17:02:13 -0500

    people

--- controllers/people.js
+++ controllers/people.js
@@ -8,13 +8,12 @@ peopleRouter.get("/all", async (req, res) => {
   res.json(persons);
 });
 
-peopleRouter.post("/", async (req, res) => {
-  const john = await Person.create({
-    name: "john",
-    phoneNumber: "828-555-1212",
-  });
-  console.log("johns auto generated ID:", john.id);
-});
+// peopleRouter.post("/", async (req, res) => {
+//   const john = await Person.create({
+//     name: "john",
+//     phoneNumber: "828-555-1212",
+//   });
+// });
 
 module.exports = peopleRouter;
 
@@ -22,4 +21,16 @@ peopleRouter.delete("/:id", async (req, res) => {
   await Person.destroy({
     where: { id: req.params.id },
   });
+});
+
+peopleRouter.post("/", async (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  const newPerson = await Person.create({
+    name: body.name,
+    phoneNumber: body.phoneNumber,
+  });
+
+  res.json(newPerson);
 });
--- models/person.js
+++ models/person.js
@@ -1,21 +1,21 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../utils/database');
+const Sequelize = require("sequelize");
+const sequelize = require("../utils/database");
 
-const Person = sequelize.define('person', {
-	id: {
-		type: Sequelize.INTEGER,
-		autoIncrement: true,
-		allowNull: false,
-		primaryKey: true,
-	},
-	name: {
-		type: Sequelize.STRING,
-		allowNull: false,
-	},
-	phoneNumber: {
-		type: Sequelize.INTEGER,
-		allowNull: false,
-	},
+const Person = sequelize.define("people", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  phoneNumber: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
 });
 
 module.exports = Person;
