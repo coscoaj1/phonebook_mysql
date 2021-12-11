058bf8106a1f7f4e8be805d715dab115f946e6b3
coscoaj1 <coscoanthony1@gmail.com>
2021-12-11 13:49:13 -0500

    deleting

--- controllers/people.js
+++ controllers/people.js
@@ -1,17 +1,25 @@
-const peopleRouter = require('express').Router();
-const Person = require('../models/Person');
+const peopleRouter = require("express").Router();
+const { response } = require("express");
+const Person = require("../models/Person");
 
-peopleRouter.get('/all', async (req, res) => {
-	const people = await Person.findAll();
-	console.log(people);
+peopleRouter.get("/all", async (req, res) => {
+  const persons = await Person.findAll();
+  console.log(persons);
+  res.json(persons);
 });
 
-peopleRouter.post('/', async (req, res) => {
-	const john = await Person.create({
-		name: 'john',
-		phoneNumber: '828-555-1212',
-	});
-	console.log('johns auto generated ID:', john.id);
+peopleRouter.post("/", async (req, res) => {
+  const john = await Person.create({
+    name: "john",
+    phoneNumber: "828-555-1212",
+  });
+  console.log("johns auto generated ID:", john.id);
 });
 
 module.exports = peopleRouter;
+
+peopleRouter.delete("/:id", async (req, res) => {
+  await Person.destroy({
+    where: { id: req.params.id },
+  });
+});
--- utils/database.js
+++ utils/database.js
@@ -1,13 +1,14 @@
-const Sequelize = require('sequelize');
-
+require("dotenv").config();
+const Sequelize = require("sequelize");
+console.log("process env user:", process.env.USER);
 const sequelize = new Sequelize(
-	process.env.DATABASE,
-	process.env.USER,
-	process.env.PASSWORD,
-	{
-		dialect: 'mysql',
-		host: process.env.HOST,
-	}
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.HOST,
+  }
 );
 
 module.exports = sequelize;
